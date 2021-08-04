ee301abcc0584990388eb331befcd2cc7c5b1a88
Isuru Rajitha <isuru.rajitha.gamage@gmail.com>
2021-08-04 17:09:19 +0530

    angular connection error fixed!

--- src/main/java/com/example/schoolmanagement/controller/StudentController.java
+++ src/main/java/com/example/schoolmanagement/controller/StudentController.java
@@ -3,12 +3,13 @@ package com.example.schoolmanagement.controller;
 import com.example.schoolmanagement.model.Students;
 import com.example.schoolmanagement.repository.StudentsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-
+@CrossOrigin(origins = "http://localhost:4200/")
 @RestController
 @RequestMapping("/api/v1/")
 public class StudentController {
